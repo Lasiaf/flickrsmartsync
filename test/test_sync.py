7c0fca14b74debe98537499695b5978e91126813
Antonio Aloisio <gnuton@gnuton.org>
2015-08-18 14:17:42 +0300

    Made tests PEP-8 compliant

--- test/test_sync.py
+++ test/test_sync.py
@@ -14,7 +14,9 @@ logger.setLevel(logging.WARNING)
 
 fakestat = os.stat(__file__)
 fakeid = 45
-class fakeLocal:
+
+
+class FakeLocal:
     files = {}
     def __init__(self):
         self.files.clear()
@@ -22,67 +24,75 @@ class fakeLocal:
     
     def build_photo_sets(self, specific_path, exts):
         return self.files
-    
-class fakeRemote:
+
+
+class FakeRemote:
     def __init__(self):
         self.photo_sets_map = {"dirname": "12345"}
-        self.files = {"12345": {"file3.jpg": 23, "file4.avi": 23}}  
+        self.files = {"12345": {"file3.jpg": 23, "file4.avi": 23}}
+
     def get_photo_sets(self):
         return self.photo_sets_map
-    def get_custom_set_title(self, path):
-        return path.split('/').pop()      
+
+    @staticmethod
+    def get_custom_set_title(path):
+        return path.split('/').pop()
+
     def get_photos_in_set(self, folder, get_url=False):
-        return self.files[self.photo_sets_map[folder]]  
-    def upload(self, file_path, photo, folder):
+        return self.files[self.photo_sets_map[folder]]
+
+    def upload(self, _, photo, folder):
         self.files[self.photo_sets_map[folder]][photo] = fakeid
         return fakeid
 
-    def download(self, url, path):
-        fakeLocal.files.values()[0].append((os.path.basename(path), fakestat))
+    @staticmethod
+    def download(_, path):
+        FakeLocal.files.values()[0].append((os.path.basename(path), fakestat))
 
-class syncTest(unittest.TestCase):
+
+class SyncTest(unittest.TestCase):
     maxDiff=None
 
     def setUp(self):
-        class args:
+        class Args:
             sync_path=here+os.sep
             custom_set=None
             ignore_images=False
             ignore_videos=False
             ignore_ext=None
-
             is_windows=False
             download="."
             sync_from="all"
-        self.local = fakeLocal()
-        self.remote = fakeRemote()
-        self.sync = Sync(args(), self.local, self.remote)
+
+        self.local = FakeLocal()
+        self.remote = FakeRemote()
+        self.sync = Sync(Args(), self.local, self.remote)
 
     def tearDown(self):
         pass
 
     def test_upload(self):
-        expected = fakeRemote().files
+        expected = FakeRemote().files
         for f, s in self.local.files.values()[0]:
             expected.values()[0][f] = fakeid
         self.sync.upload()
         self.assertEquals(self.remote.files, expected)
 
     def test_download(self):
-        expected = copy.deepcopy(fakeLocal().files)
+        expected = copy.deepcopy(FakeLocal().files)
         expected.values()[0] += [(x, fakestat) for x in self.remote.files.values()[0]]
         self.sync.download()
         self.assertEquals(self.local.files, expected)
 
     def test_sync(self):
-        expectedr = fakeRemote().files
+        expected_r = FakeRemote().files
         for f, s in self.local.files.values()[0]:
-            expectedr.values()[0][f] = fakeid        
-        expectedl = copy.deepcopy(fakeLocal().files)
-        expectedl.values()[0] += [(x, fakestat) for x in self.remote.files.values()[0]]
+            expected_r.values()[0][f] = fakeid
+        expected_l = copy.deepcopy(FakeLocal().files)
+        expected_l.values()[0] += [(x, fakestat) for x in self.remote.files.values()[0]]
         self.sync.sync()
-        self.assertEquals(self.remote.files, expectedr)
-        self.assertEquals(self.local.files, expectedl)
+        self.assertEquals(self.remote.files, expected_r)
+        self.assertEquals(self.local.files, expected_l)
         
 if __name__ == '__main__':
     logging.debug('Started test case')
